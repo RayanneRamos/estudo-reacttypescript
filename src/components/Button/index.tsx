76e7afc84715a55e7782009cb393753c387ceb6a
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-05-09 16:33:17 -0300

    Criando o componente Button e a sua estilização

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './Button.module.scss';
+
+const Button = () => {
+  return(
+    <button className={style.botao}>Enviar</button>
+  );
+}
+
+export default Button;
