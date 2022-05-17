72de735d2da48e249162feaba47af249eb7dbc34
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-05-17 15:11:01 -0300

    Criando o componente Timer e a sua estilização

---
+++ src/components/StopWatch/Timer/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import style from './Timer.module.scss';
+
+const Timer = () => {
+  return (
+    <>
+      <span className={style.relogioNumero}>0</span>
+      <span className={style.relogioNumero}>0</span>
+      <span className={style.relogioDivisao}>:</span>
+      <span className={style.relogioNumero}>0</span>
+      <span className={style.relogioNumero}>0</span>
+    </>
+  );
+}
+
+export default Timer;
