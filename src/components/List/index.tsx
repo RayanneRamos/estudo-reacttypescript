9ee2389e4f3f02fe45dc9f5463205de8cc6a737e
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-05-17 14:54:18 -0300

    Importando o componente Item e usando na Lista

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Item from "./Item";
 import style from './List.module.scss';
 
 const List = () => {
@@ -23,10 +24,10 @@ const List = () => {
       <h2>Estudos do dia</h2>
       <ul>
         {tarefas.map((item, index) => (
-          <li key={index} className={style.item}>
-            <h3>{item.tarefa}</h3>
-            <span>{item.tempo}</span>
-          </li>
+          <Item 
+            key={index}
+            {... item}
+          />
         ))}
       </ul>
     </aside>
