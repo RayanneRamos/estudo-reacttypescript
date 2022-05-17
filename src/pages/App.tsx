90e068361951cb57397b184645b173809cc7e38e
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-05-17 16:32:58 -0300

    Passando as props

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from '../components/Form';
 import List from '../components/List';
 import StopWatch from '../components/StopWatch';
+import { TarefasProps } from '../types/tarefa';
 import style from './App.module.scss';
 
 function App() {
+
+  const [ tarefas, setTarefas ] = useState<TarefasProps | []>([]);
+
   return (
     <>
       <div className={style.AppStyle}>
-        <Form />
-        <List />
+        <Form setTarefas={setTarefas} />
+        <List tarefas={tarefas} />
         <StopWatch />
       </div> 
     </>
