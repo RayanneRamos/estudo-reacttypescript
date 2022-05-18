5544f3995577f2dc0d14e07ab7b9d8aec42ef776
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-05-18 15:57:36 -0300

    Criação da função adicionarTarefa

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -2,28 +2,39 @@ import React, { useState } from "react";
 import { TarefasProps } from "../../types/tarefa";
 import Button from "../Button";
 import style from './Form.module.scss';
+import { v4 as uuidv4 } from 'uuid';
 
-type FormProps = {
+interface Props {
   setTarefas: React.Dispatch<React.SetStateAction<TarefasProps[]>>;
 }
 
-const Form = ({ setTarefas }: FormProps) => {
+const Form = ({ setTarefas }: Props) => {
 
-  const [ tarefa, setTarefa ] = useState();
-  const [ time, setTime ] = useState();
+  const [ tarefa, setTarefa ] = useState('');
+  const [ tempo, setTempo ] = useState('00:00');
 
   function adicionarTarefa(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    setTarefas((tarefasAntigas) => [...tarefasAntigas]);
+    setTarefas((tarefasAntigas) => [
+      ...tarefasAntigas, {
+        tarefa,
+        tempo,
+        selecionado: false,
+        completado: false,
+        id: uuidv4(),
+      }
+    ]);
+    setTarefa("");
+    setTempo("");
   }
 
   return (
-    <form className={style.novaTarefa} onSubmit={adicionarTarefa.bind}>
+    <form className={style.novaTarefa} onSubmit={adicionarTarefa}>
       <div className={style.inputContainer}>
         <label htmlFor="tarefa">Adicione um novo estudo</label>
         <input 
           value={tarefa}
-          //onChange={(event) => setTarefa(event.target.value)}
+          onChange={(event) => setTarefa(event.target.value)}
           type="text" 
           name="tarefa" 
           id="tarefa" 
@@ -34,8 +45,8 @@ const Form = ({ setTarefas }: FormProps) => {
       <div className={style.inputContainer}>
         <label htmlFor="tempo">Tempo</label>
         <input 
-          value={time}
-          //onChange={(event) => setTime(event.target.value)}
+          value={tempo}
+          onChange={(event) => setTempo(event.target.value)}
           type="time" 
           step="1"
           name="tempo"
