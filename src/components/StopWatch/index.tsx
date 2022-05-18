c02cbea775e996d47d38ea6b287ac0bfdc02f4d0
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-05-18 15:59:56 -0300

    Criando a função regressiva e o useEffect

--- src/components/StopWatch/index.tsx
+++ src/components/StopWatch/index.tsx
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '../Button';
 import Timer from './Timer';
 import style from './StopWatch.module.scss';
+import { TarefasProps } from '../../types/tarefa';
+import { tempoParaSegundos } from '../../common/utils/time';
 
-const StopWatch = () => {
+interface Props {
+  selecionado: TarefasProps | undefined;
+  finalizarTarefa: () => void;
+}
+
+const StopWatch = ({ selecionado, finalizarTarefa }: Props) => {
+
+  const [ tempo, setTempo ] = useState<number>();
+
+  useEffect(() => {
+    if(selecionado?.tempo) {
+      setTempo(tempoParaSegundos(selecionado.tempo));
+    }
+  }, [selecionado]);
+
+  function regressiva(contador: number = 0) {
+    setTimeout(() => {
+      if(contador > 0) {
+        setTempo(contador - 1);
+        return regressiva(contador - 1);
+      }
+      finalizarTarefa();
+    }, 1000);
+  }
+
   return(
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
       <div className={style.relogioWrapper}>
-        <Timer />
+        <Timer tempo={tempo} />
       </div>
       <Button 
+        onClick={() => regressiva(tempo)}
         text="Iniciar"
       />
     </div>
