94410c4735d9336f5185fd388758747db4f1cdac
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-05-18 16:00:20 -0300

    Criando as funções selecionaTarefa e finalizarTarefa

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -7,14 +7,39 @@ import style from './App.module.scss';
 
 function App() {
 
-  const [ tarefas, setTarefas ] = useState<TarefasProps | []>([]);
+  const [ tarefas, setTarefas ] = useState<TarefasProps[]>([]);
+  const [ selecionado, setSelecionado ] = useState<TarefasProps>();
+
+  function selecionaTarefa(tarefaSelecionada: TarefasProps) {
+    setSelecionado(tarefaSelecionada);
+    setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => ({
+      ...tarefa,
+      selecionado: tarefa.id === tarefaSelecionada.id ? true : false,
+    })));
+  }
+
+  function finalizarTarefa() {
+    if(selecionado) {
+      setSelecionado(undefined);
+      setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => {
+        if(tarefa.id === selecionado.id) {
+          return {
+            ...tarefa,
+            selecionado: false,
+            completado: true,
+          }
+        } 
+        return tarefa; 
+      }));
+    }
+  }
 
   return (
     <>
       <div className={style.AppStyle}>
         <Form setTarefas={setTarefas} />
-        <List tarefas={tarefas} />
-        <StopWatch />
+        <List tarefas={tarefas} selecionaTarefa={selecionaTarefa} />
+        <StopWatch selecionado={selecionado} finalizarTarefa={finalizarTarefa} />
       </div> 
     </>
   );
